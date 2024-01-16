535307c900d9cbab8f8a3c8cf8bea5598412f4bf
ok-dom <44346151+ok-dom@users.noreply.github.com>
2024-01-17 02:30:34 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,7 @@
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
