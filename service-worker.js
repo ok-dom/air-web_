c9421d5d892bdf00d1c7412583aa34895d3d391a
ok-dom <44346151+ok-dom@users.noreply.github.com>
2024-01-17 16:31:26 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,35 +5,3 @@ self.addEventListener('fetch', function(event) {
     })
   );
 });
-
-// Слушаем событие 'push'.
-self.addEventListener('push', function(event) {
-  event.waitUntil(self.registration.showNotification('Your mind', {
-    body: 'Wizard invaded to your mind!'
-  }));
-});
-
-// Слушаем событие 'pushsubscriptionchange', которое запускается,
-// когда истекает срок подписки. 
-// Подписываемся снова и регистрируем новую подписку на сервере,
-// отправив POST-запрос.
-// На боевом приложении скорее всего будет использоваться ID или token
-// для идентификации пользователя.
-self.addEventListener('pushsubscriptionchange', function(event) {
-  console.log('Spell expired');
-  event.waitUntil(
-    self.registration.pushManager.subscribe({ userVisibleOnly: true })
-    .then(function(subscription) {
-      console.log('Another invade! Legilimens!', subscription.endpoint);
-      return fetch('register', {
-        method: 'post',
-        headers: {
-          'Content-type': 'application/json'
-        },
-        body: JSON.stringify({
-          endpoint: subscription.endpoint
-        })
-      });
-    })
-  );
-});
