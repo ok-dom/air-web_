ec740e65b9af001bde148fb9d376940467c5d067
ok-dom <44346151+ok-dom@users.noreply.github.com>
2024-01-19 10:57:23 +0300

    Update push.js

--- push.js
+++ push.js
@@ -1,6 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
 import { getMessaging, getToken, onMessage } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-messaging.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCKCvm_5irAQ_GIt9w8mK-evbThi4Vupyc",
+  authDomain: "ezon-1dedf.firebaseapp.com",
+  projectId: "ezon-1dedf",
+  storageBucket: "ezon-1dedf.appspot.com",
+  messagingSenderId: "249841035094",
+  appId: "1:249841035094:web:2b7a502354ae5582bb18f7",
+  measurementId: "G-X9RHNJT62H"
+};
+
 const firebaseApp = initializeApp(firebaseConfig);
 const firebaseMessaging = getMessaging(firebaseApp);
 
