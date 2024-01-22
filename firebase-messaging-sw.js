cbf149ec4e2f491154dec8272947ae993faea8bf
ok-dom <44346151+ok-dom@users.noreply.github.com>
2024-01-22 16:01:53 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -25,20 +25,3 @@ messaging.onBackgroundMessage(function(payload) {
   };
    self.registration.showNotification(notificationTitle, notificationOptions);
 });
-/*
-importScripts("https://www.gstatic.com/firebasejs/10.7.2/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.7.2/firebase-messaging-compat.js");
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCKCvm_5irAQ_GIt9w8mK-evbThi4Vupyc",
-  authDomain: "ezon-1dedf.firebaseapp.com",
-  projectId: "ezon-1dedf",
-  storageBucket: "ezon-1dedf.appspot.com",
-  messagingSenderId: "249841035094",
-  appId: "1:249841035094:web:2b7a502354ae5582bb18f7",
-  measurementId: "G-X9RHNJT62H"
-};
-
-firebase.initializeApp(firebaseConfig);
-*/
